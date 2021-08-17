6c363cf408b55725119b2b6768a27df220383522
SpearJin <87363129+SpearJin@users.noreply.github.com>
2021-08-17 19:06:09 +0900

    habit: State

--- project/src/components/habit.jsx
+++ project/src/components/habit.jsx
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
 
 class Habit extends Component {
+  state = {
+    count: 0,
+  };
+
+  handleIncrease = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  handleDecrease = () => {
+    const count = this.state.count;
+    this.setState({ count: count <= 0 ? 0 : count - 1 });
+  };
+
+  handleDelete = () => {};
+
   render() {
     return (
       <div className='habit'>
         <span className='habit-name'>Reading</span>
-        <span className='habit-count'>0</span>
-        <button className='habit-button habit-increase'>
+        <span className='habit-count'>{this.state.count}</span>
+        <button
+          className='habit-button habit-increase'
+          onClick={this.handleIncrease}
+        >
           <i className='fas fa-plus-square'></i>
         </button>
-        <button className='habit-button habit-decrease'>
+        <button
+          className='habit-button habit-decrease'
+          onClick={this.handleDecrease}
+        >
           <i className='fas fa-minus-square'></i>
         </button>
-        <button className='habit-button habit-delete'>
+        <button
+          className='habit-button habit-delete'
+          onClick={this.handleDelete}
+        >
           <i className='fas fa-trash'></i>
         </button>
       </div>
